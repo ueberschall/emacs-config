7a3a6d4aaf090238983f7e806287e02f73061aa4
zufall <nasser.attar@hotmail.com>
2019-02-03 22:31:41 +0100

    Ein paar Tastenkombinationen für den C/C++ Modus hinzugefuegt

--- custom/cpp_ide/setup-cpp-keymap.el
+++ custom/cpp_ide/setup-cpp-keymap.el
@@ -5,8 +5,13 @@
 (define-key c-mode-base-map (kbd "M-#") 'rtags-location-stack-forward)
 (define-key c-mode-base-map (kbd "M-'") 'rtags-location-stack-back)
 (define-key c-mode-base-map (kbd "M-?") 'rtags-display-summary)
+(define-key c-mode-base-map (kbd "M-;") 'rtags-find-file)
+(define-key c-mode-base-map (kbd "M-i") 'rtags-imenu)
+(define-key c-mode-base-map (kbd "C-.") 'rtags-find-symbol)
+(define-key c-mode-base-map (kbd "C-,") 'rtags-find-references)
+(define-key c-mode-base-map (kbd "C-<") 'rtags-find-virtuals-at-point)
 
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
-(define-key c-mode-base-map (kbd "C-x 1") 'clang-format-region)
+(define-key c-mode-base-map (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
 
 (provide 'setup-cpp-keymap)
