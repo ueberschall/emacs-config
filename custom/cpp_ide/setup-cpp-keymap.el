2c2ac589c48e200a8b6d08e6e7c61262212be28e
zufall <nasser.attar@hotmail.com>
2019-02-04 20:27:41 +0100

    Tastenkombination für RTags hinzugefuegt

--- custom/cpp_ide/setup-cpp-keymap.el
+++ custom/cpp_ide/setup-cpp-keymap.el
@@ -10,6 +10,7 @@
 (define-key c-mode-base-map (kbd "C-.") 'rtags-find-symbol)
 (define-key c-mode-base-map (kbd "C-,") 'rtags-find-references)
 (define-key c-mode-base-map (kbd "C-<") 'rtags-find-virtuals-at-point)
+(define-key c-mode-base-map (kbd "C-c r r") 'rtags-rename-symbol)
 
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 (define-key c-mode-base-map (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
