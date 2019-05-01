51429182a9f14d8187f182469f4a0afa7736ea2d
Nasser Attar <nasser.attar@hotmail.com>
2019-05-01 23:53:50 +0200

    Removed the unnecessary progn command in the package configuration of C++ and Python

--- custom/cpp_ide/setup-cpp-packages.el
+++ custom/cpp_ide/setup-cpp-packages.el
@@ -1,54 +1,48 @@
 (use-package cc-mode
   :init
-  (progn
-    (c-add-style "ana_style"
-                 '("bsd"
-                   (c-basic-offset . 4)
-                   (c-offset-alists
-                    (case-label . *)
-                    (innamespace . 0))))
+  (c-add-style "ana_style"
+               '("bsd"
+                 (c-basic-offset . 4)
+                 (c-offset-alists
+                  (case-label . *)
+                  (innamespace . 0))))
 
-    (setq c-default-style "ana_style")
-    (add-hook 'c-mode-common-hook (lambda ()
-                                    (c-set-style "ana_style")))
-    (add-hook 'c++-mode-hook (lambda ()
-                               (c-set-offset 'innamespace 0)))))
-
-;; (use-package semantic
-;;   :init
-;;   (progn
-;;     (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
-;;     (semantic-mode 1)))
+  (setq c-default-style "ana_style")
+  (add-hook 'c-mode-common-hook (lambda ()
+                                  (c-set-style "ana_style")))
+  (add-hook 'c++-mode-hook (lambda ()
+                             (c-set-offset 'innamespace 0))))
 
 (use-package rtags
   :config
-  (progn
-    (setq rtags-path "/usr/local/bin")
-    (unless (rtags-executable-find "rc") (error "Binary rc is not installed!"))
-    (unless (rtags-executable-find "rdm") (error "Binary rdm is not installed!"))
+  (setq rtags-path "/usr/local/bin")
+  (unless (rtags-executable-find "rc") (error "Binary rc is not installed!"))
+  (unless (rtags-executable-find "rdm") (error "Binary rdm is not installed!"))
 
-    (setq rtags-use-helm t)
+  (setq rtags-use-helm t)
 
-    ;; Shutdown rdm when leaving emacs.
-    (add-hook 'kill-emacs-hook 'rtags-quit-rdm)
-    (rtags-start-process-unless-running)))
+  ;; Shutdown rdm when leaving emacs.
+  (add-hook 'kill-emacs-hook 'rtags-quit-rdm)
 
-(use-package helm-rtags
+  (use-package helm-rtags
   :requires helm rtags
   :config
-  (progn
-    (setq rtags-display-result-backend 'helm)))
+  (setq rtags-display-result-backend 'helm))
 
-(use-package company-rtags
+  (use-package company-rtags
   :requires company rtags
   :config
-  (progn
   (setq rtags-autostart-diagnostics t)
   (rtags-diagnostics)
   (push 'company-rtags company-backends)
-  (setq rtags-completions-enabled t)))
+  (setq rtags-completions-enabled t))
+
+  (rtags-start-process-unless-running))
 
 (require 'flycheck-rtags)
+
+;; (require 'rtags)
+;; (cmake-ide-setup)
 
 (use-package zygospore)
 
--- custom/python_ide/setup-python-package.el
+++ custom/python_ide/setup-python-package.el
@@ -9,17 +9,16 @@
 (use-package flycheck)
 
 (use-package elpy
-  :init
-  (progn
-    (add-hook 'elpy-mode-hook 'flycheck-mode)
-    (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
   :config
-  (progn
-    (setq python-shell-interpreter "jupyter"
-          python-shell-interpreter-args "console --simple-prompt"
-          python-shell-prompt-detect-failure-warning nil)
-    (add-to-list 'python-shell-completion-native-disabled-interpreters
-                 "jupyter")
-    (elpy-enable)))
+  (setq python-shell-interpreter "jupyter"
+        python-shell-interpreter-args "console --simple-prompt"
+        python-shell-prompt-detect-failure-warning nil)
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+               "jupyter")
+
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+  
+  (elpy-enable))
 
 (provide 'setup-python-package)
