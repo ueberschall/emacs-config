c25df5821f39f0daa5e49e7d68beede426ff03cb
Nasser Attar <abdul.attar@ibeo-as.com>
2019-10-16 17:14:58 +0200

    Den Auto-Save-Hook verbessert

--- custom/cpp_ide/setup-cpp-ana-func.el
+++ custom/cpp_ide/setup-cpp-ana-func.el
@@ -3,4 +3,8 @@
   (when (file-exists-p (expand-file-name ".clang-format" (projectile-project-root)))
     (clang-format-buffer)))
 
+(defun clang-format-buffer-smart-on-save ()
+  "Add auto-save hook for clang-format-buffer-smart."
+  (add-hook 'before-save-hook 'clang-format-buffer-smart nil t))
+
 (provide 'setup-cpp-ana-func)
