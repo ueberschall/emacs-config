6cc90f09657c52a1c098c083d5686d4633b99f0f
Nasser Attar <nasser.attar@hotmail.com>
2019-11-09 13:46:31 +0100

    Dinge in der Konfig von Tex geändert

--- custom/tex/setup-tex-package.el
+++ custom/tex/setup-tex-package.el
@@ -1,3 +1,5 @@
-
+;; Das Feature 'auctex' wird vom package nicht provided. Deshalb ist es
+;; hier auskommentiert, da use-package nur einen Fehler werfen würde.
+;(use-package auctex)
 
 (provide 'setup-tex-package)
