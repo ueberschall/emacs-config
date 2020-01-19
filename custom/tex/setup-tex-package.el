2efd308fe88368df3384b24657571dd3c85b7bb0
Nasser Attar <nasser.attar@hotmail.com>
2020-01-19 10:43:10 +0100

    Installiere Tex-Packages wenn nicht schon geschehen

--- custom/tex/setup-tex-package.el
+++ custom/tex/setup-tex-package.el
@@ -1,3 +1,6 @@
+;; Installiere Auctex, wenn nicht schon geschehen.
+(install-necessary-packages '(auctex))
+
 ;; Das Feature 'auctex' wird vom package nicht provided. Deshalb ist es
 ;; hier auskommentiert, da use-package nur einen Fehler werfen würde.
 ;(use-package auctex)
