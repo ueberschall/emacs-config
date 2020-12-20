0819c6f741966f54fa35cf5049263c8bbe76c71c
Nasser Attar <abdul.attar@ibeo-as.com>
2020-12-20 10:45:14 +0100

    Refactor the comments in init.el

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
 ;; ~/.emacs.d/init.el
 ;; Erstellt: 		14. April 2017
-;; Aktualisiert: 	31. Januar 2019
+;; Aktualisiert: 	20. Dezember 2020
 ;;
 ;; Die Init-Datei, die als Einsprungspunkt dient
-;;
-;;
-;; *** Ergänze die Suchpfade ***
+
+;; Ergänze die Suchpfade 
 (add-to-list 'load-path "~/.emacs.d/dired+")
 (add-to-list 'load-path "~/.emacs.d/modeline-posn")
 (add-to-list 'load-path "~/.emacs.d/custom/basic")
@@ -15,17 +14,17 @@
 (add-to-list 'load-path "~/.emacs.d/custom/tex")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/custom/themes")
 
-;; *** Laden der Basis-Konfiguration ***
+;; Laden der Basis-Konfiguration
 (require 'setup-basic)
 
-;; *** Lade die Einstellungen für eine bestimmte Programmiersprache ***
+;; Für jede Programmiersprache gibt es einen bestimmten IDE-Modus
 (setq ide-mode (load-ide-mode))
 
-;; *** Die Fensterleiste wird so eingestellt, dass der Name des aktuellen Buffers
-;; *** angezeigt wird ***
+;; Die Fensterleiste wird so eingestellt, dass der aktuelle IDE-Modus
+;; zusammen mit der aktuellen Emacs-Version angezeigt wird.
 (setq frame-title-format
       (list ide-mode "   -   Emacs " emacs-version))
 
-;; *** Laden des "tron"-Themes, das aus den offiziellen Packetquellen stammt ***
+;; Laden des "tron"-Themes, das aus den offiziellen Packetquellen stammt
 (setq custom-safe-themes t)
 (add-hook 'after-init-hook (lambda () (load-theme 'tron)))
