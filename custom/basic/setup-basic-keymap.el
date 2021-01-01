022394555c558cbcf76c9cb1d5f9cc939e5053e1
Nasser Attar <abdul.attar@ibeo-as.com>
2021-01-01 18:23:13 +0100

    Refactor basic custom key definitions

--- custom/basic/setup-basic-keymap.el
+++ custom/basic/setup-basic-keymap.el
@@ -1,10 +1,8 @@
-;; *** Lege das Kompilier-Kommando auf <f5> ***
+;; Global Set
 (global-set-key (kbd "<f5>") (lambda ()
                                (interactive)
                                (setq-local compilation-read-command nil)
                                (call-interactively 'compile)))
-
-;; *** Global Set ***
 (global-set-key (kbd "C-c w") 'ace-window)
 (global-set-key (kbd "<C-right>") 'windmove-right)
 (global-set-key (kbd "<C-left>") 'windmove-left)
@@ -16,7 +14,8 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
-(global-set-key (kbd "C-x C-b") 'ibuffer) ;; Stattdessen den entsprechenden Helm-Command wählen.
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
@@ -28,47 +27,54 @@
 (global-set-key (kbd "C-c r") 'helm-recentf)
 (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-c h o") 'helm-occur)
-
 (global-set-key (kbd "C-c h w") 'helm-wikipedia-suggest)
 (global-set-key (kbd "C-c h g") 'helm-google-suggest)
-
 (global-set-key (kbd "C-c h x") 'helm-register)
+
 (global-set-key (kbd "<C-tab>") 'company-complete)
 
-(global-set-key (kbd "C-c C-d") 'duplicate-line)
+(global-set-key (kbd "C-c <return>") 'duplicate-line)
 
-;; *** Semantic Refactor ***
 (global-set-key (kbd "M-RET o") 'srefactor-lisp-one-line)
 (global-set-key (kbd "M-RET m") 'srefactor-lisp-format-sexp)
 (global-set-key (kbd "M-RET d") 'srefactor-lisp-format-defun)
 (global-set-key (kbd "M-RET b") 'srefactor-lisp-format-buffer)
 
-;; *** Global Unset ***
-(global-unset-key (kbd "C-x c"))
+;; Global Unset
+(global-unset-key (kbd "C-x c")) ;; Recommended in Helm tutorial, see http://tuhdo.github.io/helm-intro.html
 (global-unset-key (kbd "C-x f"))
 
-;; *** Help Commands ***
+;; Help Commands
 (define-key 'help-command (kbd "C-f") 'helm-apropos)
 (define-key 'help-command (kbd "r") 'helm-info-emacs)
 (define-key 'help-command (kbd "C-l") 'helm-locate-library)
 
-;; *** Mini Buffer ***
+;; Mini Buffer
 (define-key minibuffer-local-map (kbd "M-p") 'helm-minibuffer-history)
 (define-key minibuffer-local-map (kbd "M-n") 'helm-minibuffer-history)
 
-;; *** Shell Mode ***
+;; Helm Mode
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+
+;; Shell Mode
 (define-key shell-mode-map (kbd "<C-right>") 'windmove-right)
 (define-key shell-mode-map (kbd "<C-left>") 'windmove-left)
 (define-key shell-mode-map (kbd "<C-up>") 'windmove-up)
 (define-key shell-mode-map (kbd "<C-down>") 'windmove-down)
 
-;; *** Dired Mode ***
+;; Dired Mode
 (define-key dired-mode-map (kbd "<C-right>") 'windmove-right)
 (define-key dired-mode-map (kbd "<C-left>") 'windmove-left)
 (define-key dired-mode-map (kbd "<C-up>") 'windmove-up)
 (define-key dired-mode-map (kbd "<C-down>") 'windmove-down)
 
-;; *** Org Mode ***
+;; Org Mode
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
