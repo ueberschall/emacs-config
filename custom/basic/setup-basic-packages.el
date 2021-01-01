1e6defd7aadf0cb334aca1ad8b7625cdb50edfb9
Nasser Attar <abdul.attar@ibeo-as.com>
2021-01-01 18:23:34 +0100

    Automate installation of dired+ and modeline-posn packages

--- custom/basic/setup-basic-packages.el
+++ custom/basic/setup-basic-packages.el
@@ -1,4 +1,4 @@
-;; *** Installieren der notwendigen Packages, falls dies noch nicht getan wurde ***
+;; Installieren der notwendigen Packages, falls dies noch nicht getan wurde
 (require 'package)
 
 (add-to-list 'package-archives
@@ -9,10 +9,7 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; *** Installiere fehlende Packete ***
-;; dired+ ist hier nicht aufgeführt weil,
-;; es nur manuell installiert werden kann und
-;; nicht aus den Emacs-Packetquellen
+;; Installiere fehlende Packete
 (install-necessary-packages '(use-package
                                ace-window
                                exec-path-from-shell
@@ -26,7 +23,26 @@
                                org
                                ))
 
-;; *** Lade benötigte Packete und Dateien ***
+;; dired+ und modeline-posn werden von EmacsWiki heruntergeladen
+(let* ((diredplus-file (expand-file-name "dired+/dired+.el" "~/.emacs.d"))
+      (diredplus-dir (file-name-directory diredplus-file)))
+  (if (file-exists-p diredplus-file)
+    (message "Dired+ does already exist")
+  (unless (file-directory-p diredplus-dir)
+    (make-directory diredplus-dir))
+  (url-copy-file "https://www.emacswiki.org/emacs/download/dired%2b.el"
+                 diredplus-file)))
+
+(let* ((modeline-file (expand-file-name "modeline-posn/modeline-posn.el" "~/.emacs.d"))
+      (modeline-dir (file-name-directory modeline-file)))
+  (if (file-exists-p modeline-file)
+    (message "Modeline-Posn does already exist")
+  (unless (file-directory-p modeline-dir)
+    (make-directory modeline-dir))
+  (url-copy-file "https://www.emacswiki.org/emacs/download/modeline-posn.el"
+                 modeline-file)))
+
+;; Lade benötigte Packete und Dateien
 (require 'use-package)
 
 (use-package ace-window)
@@ -34,13 +50,6 @@
 (use-package magit)
 (use-package srefactor-lisp)
 (use-package org)
-
-(use-package dired+
-  :config (diredp-toggle-find-file-reuse-dir 1))
-
-;; Das ist ein manuell kopiertes Package. "use-package" funktioniert
-;; irgendwie nicht.
-(require 'modeline-posn)
 
 (use-package sr-speedbar
   :init (setq speedbar-show-unknown-files t)
@@ -59,61 +68,52 @@
   :config (projectile-mode 1))
 
 (use-package helm
-  :init
-  (progn
-    (require 'helm-config)
-    (require 'helm-grep)
-    ;; To fix error at compile:
-    ;; Error (bytecomp): Forgot to expand macro with-helm-buffer in
-    ;; (with-helm-buffer helm-echo-input-in-header-line)
-    (if (version< "26.0.50" emacs-version)
-        (eval-when-compile (require 'helm-lib)))
-
-    (setq helm-echo-input-in-header-line t)
-    
-    (defun helm-hide-minibuffer-maybe ()
-      (when (with-helm-buffer helm-echo-input-in-header-line)
-        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-          (overlay-put ov 'window (selected-window))
-          (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
-                                  `(:background ,bg-color :foreground ,bg-color)))
-          (setq-local cursor-type nil))))
-
-    (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
-
-    (setq helm-split-window-in-side-p t
-;;          helm-autoresize-max-height 40
-;;          helm-autoresize-min-height 20
-          helm-mode-fuzzy-match t
-          helm-buffers-fuzzy-matching t
-          helm-M-x-fuzzy-match t
-          helm-imenu-fuzzy-match t
-          helm-lisp-fuzzy-completion t
-          helm-locate-fuzzy-match t)
-
-    (when (executable-find "curl")
-      (setq helm-google-suggest-use-curl-p t))
-
-    (helm-autoresize-mode -1)
-
-    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-    (define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
-    (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
-    (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
-
-    (use-package helm-projectile
-      :init
-      (helm-projectile-on)
-      (setq projectile-completion-system 'helm)
-      (setq projectile-indexing-method 'alien)))
-
   :config
+  (require 'helm-config)
+  (require 'helm-grep)
+
+  (setq helm-echo-input-in-header-line t)
+  
+  (defun helm-hide-minibuffer-maybe ()
+    (when (with-helm-buffer helm-echo-input-in-header-line)
+      (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+        (overlay-put ov 'window (selected-window))
+        (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                                `(:background ,bg-color :foreground ,bg-color)))
+        (setq-local cursor-type nil))))
+
+  (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+
+  (setq helm-split-window-in-side-p t
+        helm-mode-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-M-x-fuzzy-match t
+        helm-imenu-fuzzy-match t
+        helm-lisp-fuzzy-completion t
+        helm-locate-fuzzy-match t)
+
+  (when (executable-find "curl")
+    (setq helm-google-suggest-use-curl-p t))
+
+  (helm-autoresize-mode -1)
+
   (set-face-attribute 'helm-source-header nil :foreground "dark magenta" :weight 'bold
                       :background "black" :font "Ubuntu Mono-14")
   (set-face-attribute 'helm-selection nil :foreground "white" :background "SpringGreen4")
   (set-face-attribute 'helm-buffer-modified nil :foreground "RosyBrown"))
+
+(use-package helm-projectile
+  :after helm
+  :init
+  (helm-projectile-on)
+  (setq projectile-completion-system 'helm)
+  (setq projectile-indexing-method 'alien))
+
+(use-package dired+
+  :config (diredp-toggle-find-file-reuse-dir 1))
+
+;; Das ist ein manuell kopiertes Package. "use-package" funktioniert
+;; irgendwie nicht.
+(require 'modeline-posn)
 
 (provide 'setup-basic-packages)
