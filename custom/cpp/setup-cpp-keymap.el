af44f50542528d4a47845206c262af4a48794b28
Nasser Attar <nasser.attar@hotmail.com>
2021-10-16 00:35:41 +0200

    Add new keybindings for Doxygen comments

--- custom/cpp/setup-cpp-keymap.el
+++ custom/cpp/setup-cpp-keymap.el
@@ -11,7 +11,7 @@
 (define-key c-mode-base-map (kbd "C-;") 'rtags-find-virtuals-at-point)
 (define-key c-mode-base-map (kbd "C-c r r") 'rtags-rename-symbol)
 
-(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+(define-key c-mode-base-map (kbd "RET") 'cpp-newline)
 (define-key c-mode-base-map (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
 
 (define-key c-mode-base-map (kbd "M-RET") 'srefactor-refactor-at-point)
@@ -19,5 +19,10 @@
 ;; Clang format
 (define-key c-mode-base-map (kbd "C-c f r") 'clang-format-region)
 (define-key c-mode-base-map (kbd "C-c f b") 'clang-format-buffer)
+
+;; Doxygen
+(define-key c-mode-base-map (kbd "C-c =") 'insert-double-line-comment-seperator)
+(define-key c-mode-base-map (kbd "C-c -") 'insert-single-line-comment-seperator)
+(define-key c-mode-base-map (kbd "C-c d c") 'extract-and-insert-doxygen-documentation-for-symbol-under-point)
 
 (provide 'setup-cpp-keymap)
