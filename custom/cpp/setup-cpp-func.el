b9e8a3544710e0fa48f80f7a521a06d8aa1ae90c
Nasser Attar <nasser.attar@hotmail.com>
2021-10-23 13:28:59 +0200

    Comment out the file- and region-wise doxygen functions
    Turns out that these functions are right now a little bit complicated
    and also not really necessary. So they are dismissed right now.

--- custom/cpp/setup-cpp-func.el
+++ custom/cpp/setup-cpp-func.el
@@ -288,71 +288,71 @@ The type however is not forwarded."
                   (extract-doxygen-documentation-for-variable-after-point)))))))))))
 
 
-(defun extract-and-insert-doxygen-documentation-for-region (start end long-p)
-  "Insert doxygen documentation for every relevant expression in the passed region"
-  (save-excursion
-    (save-restriction
-      (narrow-to-region start end)
-      (goto-char (point-min))
-      (while (re-search-forward
-              c-type-regex nil t)
-        (setq selected-string (match-string-no-properties 0))
-        (if (string-match-p "/\\*" selected-string)
-            (re-search-forward "\\*/" nil)
-          (if (string-match-p "//" selected-string)
-              (end-of-line)
-            (if (string-match-p c-type-regex selected-string)
-                (if (string-match-p template-regex selected-string)
-                    (progn
-                      (re-search-backward template-regex)
-                      (insert-doxygen-block (extract-doxygen-documentation-for-template-after-point) long-p))
-                  (if (string-match-p class-regex selected-string)
-                      (progn
-                        (re-search-backward class-regex)
-                        (insert-doxygen-block (extract-doxygen-documentation-for-class-after-point) long-p))
-                    (if (string-match-p struct-regex selected-string)
-                        (progn
-                          (re-search-backward struct-regex)
-                          (insert-doxygen-block (extract-doxygen-documentation-for-struct-after-point) long-p))
-                      (if (string-match-p alias-regex selected-string)
-                          (progn
-                            (re-search-backward alias-regex)
-                            (insert-doxygen-block (extract-doxygen-documentation-for-alias-after-point) long-p))
-                        (if (looking-at-p "(.*)")
-                            (progn
-                              (re-search-backward "\\_<")
-                              (insert-doxygen-block (extract-doxygen-documentation-for-function-after-point) long-p))
-                          (when (looking-at-p "\\(?:{.*}\\)\\|;")
-                            (re-search-backward "\\_<")
-                            (insert-doxygen-block (extract-doxygen-documentation-for-variable-after-point) long-p)))))))
-              (when (string-match-p "{" selected-string)
-                (let (bound
-                      (start (point)))
-                  (save-excursion
-                    (line-move -1)
-                    (beginning-of-line)
-                    (setq bound (point)))
-                  (if (looking-back (format "%s.+\n?.*" class-regex) bound)
-                      (progn
-                        (backward-char)
-                        (forward-sexp)
-                        (extract-and-insert-doxygen-documentation-for-region start (point) nil))
-                    (backward-char)
-                    (forward-sexp)))))))))))
+;; (defun extract-and-insert-doxygen-documentation-for-region (start end long-p)
+;;   "Insert doxygen documentation for every relevant expression in the passed region"
+;;   (save-excursion
+;;     (save-restriction
+;;       (narrow-to-region start end)
+;;       (goto-char (point-min))
+;;       (while (re-search-forward
+;;               c-type-regex nil t)
+;;         (setq selected-string (match-string-no-properties 0))
+;;         (if (string-match-p "/\\*" selected-string)
+;;             (re-search-forward "\\*/" nil)
+;;           (if (string-match-p "//" selected-string)
+;;               (end-of-line)
+;;             (if (string-match-p c-type-regex selected-string)
+;;                 (if (string-match-p template-regex selected-string)
+;;                     (progn
+;;                       (re-search-backward template-regex)
+;;                       (insert-doxygen-block (extract-doxygen-documentation-for-template-after-point) long-p))
+;;                   (if (string-match-p class-regex selected-string)
+;;                       (progn
+;;                         (re-search-backward class-regex)
+;;                         (insert-doxygen-block (extract-doxygen-documentation-for-class-after-point) long-p))
+;;                     (if (string-match-p struct-regex selected-string)
+;;                         (progn
+;;                           (re-search-backward struct-regex)
+;;                           (insert-doxygen-block (extract-doxygen-documentation-for-struct-after-point) long-p))
+;;                       (if (string-match-p alias-regex selected-string)
+;;                           (progn
+;;                             (re-search-backward alias-regex)
+;;                             (insert-doxygen-block (extract-doxygen-documentation-for-alias-after-point) long-p))
+;;                         (if (looking-at-p "(.*)")
+;;                             (progn
+;;                               (re-search-backward "\\_<")
+;;                               (insert-doxygen-block (extract-doxygen-documentation-for-function-after-point) long-p))
+;;                           (when (looking-at-p "\\(?:{.*}\\)\\|;")
+;;                             (re-search-backward "\\_<")
+;;                             (insert-doxygen-block (extract-doxygen-documentation-for-variable-after-point) long-p)))))))
+;;               (when (string-match-p "{" selected-string)
+;;                 (let (bound
+;;                       (start (point)))
+;;                   (save-excursion
+;;                     (line-move -1)
+;;                     (beginning-of-line)
+;;                     (setq bound (point)))
+;;                   (if (looking-back (format "%s.+\n?.*" class-regex) bound)
+;;                       (progn
+;;                         (backward-char)
+;;                         (forward-sexp)
+;;                         (extract-and-insert-doxygen-documentation-for-region start (point) nil))
+;;                     (backward-char)
+;;                     (forward-sexp)))))))))))
 
 
 
-(defun extract-and-insert-doxygen-documentation-for-file (file)
-  "Generate doxygen documentation for file"
-    (save-excursion
-      (set-buffer (find-file-noselect file))
-      (extract-and-insert-doxygen-documentation-for-region (point-min) (point-max) t)))
+;; (defun extract-and-insert-doxygen-documentation-for-file (file)
+;;   "Generate doxygen documentation for file"
+;;     (save-excursion
+;;       (set-buffer (find-file-noselect file))
+;;       (extract-and-insert-doxygen-documentation-for-region (point-min) (point-max) t)))
 
-(defun doxygen-documentation-for-file-interactive ()
-  "Interactive version of doxygen-documentation-for-file"
-  (interactive)
-  (setq file (read-file-name "File: " nil (buffer-file-name) t nil 'is-cpp-file))
-  (extract-and-insert-doxygen-documentation-for-file file))
+;; (defun doxygen-documentation-for-file-interactive ()
+;;   "Interactive version of doxygen-documentation-for-file"
+;;   (interactive)
+;;   (setq file (read-file-name "File: " nil (buffer-file-name) t nil 'is-cpp-file))
+;;   (extract-and-insert-doxygen-documentation-for-file file))
 
 ;; Insert '//!' after inserting newline when in Doxygen comment block
 (defun cpp-newline ()
