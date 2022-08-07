ed206a4ce7c51c64e60d11283b243fcda3305493
Nasser Attar <nasser.attar@hotmail.com>
2022-08-07 23:04:48 +0200

    Move function to basic-functions.el

--- custom/basic-functions.el
+++ custom/basic-functions.el
@@ -1,1 +1,64 @@
+(defun revert-all-file-buffers ()
+  "Refresh all open file buffers without confirmation.
+Buffers in modified (not yet saved) state in emacs will not be reverted. They
+will be reverted though if they were modified outside emacs.
+Buffers visiting files which do not exist any more or are no longer readable
+will be killed."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (let ((filename (buffer-file-name buf)))
+      ;; Revert only buffers containing files, which are not modified;
+      ;; do not try to revert non-file buffers like *Messages*.
+      (when (and filename
+                 (not (buffer-modified-p buf)))
+        (if (file-readable-p filename)
+            ;; If the file exists and is readable, revert the buffer.
+            (with-current-buffer buf
+              (revert-buffer :ignore-auto :noconfirm :preserve-modes))
+          ;; Otherwise, kill the buffer.
+          (let (kill-buffer-query-functions) ; No query done when killing buffer
+            (kill-buffer buf)
+            (message "Killed non-existing/unreadable file buffer: %s" filename))))))
+  (message "Finished reverting buffers containing unmodified files."))
+
+(defun duplicate-line ()
+  "Duplicate the line up to cursor position on next line"
+  (interactive)
+  (set-mark-command nil)
+  (move-beginning-of-line nil)
+  (kill-ring-save nil nil t)
+  (move-end-of-line nil)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+  (deactivate-mark))
+
+(defun kill-all-buffers-with-parent-directory ()
+  "Kill all buffers which got a specific directory as root"
+  (interactive)
+  (save-excursion
+    (let ((directory (read-directory-name "Directory: " (expand-file-name default-directory) nil t))
+          (currentBuffers (buffer-list)))
+      (when (string-prefix-p "~/" directory)
+        (setq directory (replace-regexp-in-string "^~" (getenv "HOME") directory)))
+      (dolist (buffer currentBuffers)
+        (when (and (buffer-file-name buffer)
+                   (string-match-p (regexp-quote directory)
+                                   (buffer-file-name buffer)))
+          (kill-buffer buffer))))))
+
+(defun save-and-kill-outdated-buffers (nb-of-buffers-to-keep)
+  "Keep only the most nb-of-buffers-to-keep recent file buffers and kill the rest"
+  (save-some-buffers t)
+  (setq i 0)
+  (dolist (buf (buffer-list))
+    (when (buffer-file-name buf)
+      (setq i (+ i 1)))
+    (unless (or (eq buf (current-buffer))
+                (and (buffer-file-name buf)
+                     (<= i nb-of-buffers-to-keep))
+                (and (string-prefix-p "*" (buffer-name buf))
+                     (string-suffix-p "*" (buffer-name buf))))
+      (kill-buffer buf))))
+
 (provide 'basic-functions)
--- custom/basic-setup.el
+++ custom/basic-setup.el
@@ -1,5 +1,7 @@
 ;; Load some usability functions.
 (require 'basic-functions)
+;; Load the functions for saving and restoring desktops.
+(require 'desktop-saving)
 
 ;; Set the text in the frame title to be "Emacs <version>".
 (setq frame-title-format (concat "Emacs " emacs-version))
@@ -46,34 +48,22 @@
 (global-linum-mode t)  ;; Display row numbers.
 (column-number-mode 1) ;; Display column number of point.
 (electric-pair-mode 1) ;; Automatic closing of parentheses.
-(winner-mode 1)
-(global-visual-line-mode 1)
-(show-paren-mode 1)
-(setq show-paren-delay 0)
+(winner-mode 1) ;; Window actions can be undone.
+(global-visual-line-mode 1) ;; Visual line mode is activated globally
+(show-paren-mode 1) ;; Display paired parentheses.
+(setq show-paren-delay 0) 
 
 ;; Make 'yes-or-no' queries easier to confirm.
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Settings for tabs and indentations.
-(setq-default indent-tabs-mode nil) ;; Indentation
+(setq-default indent-tabs-mode nil) ;; Use space for indentation instead of tabs.
 (setq-default tab-width 4)
-(setq-default indent-tabs-mode nil)
 
-;; Load the functions for saving and restoring desktops.
-(require 'desktop-saving)
+;; Loading custom themes is considered safe
+(setq custom-safe-themes t)
 
-;; Speichern des Desktops (Quelle: https://www.emacswiki.org/emacs/Desktop)
-(setq desktop-load-locked-desktop t)
-
-(setq desktop-path '("~/.emacs.d/"))
-(setq desktop-dirname "~/.emacs.d/")
-(setq desktop-base-file-name "emacs-desktop")
-
-(add-hook 'desktop-after-read-hook 'delete-desktop)
-
-(add-hook 'kill-emacs-hook (lambda ()
-                             (save-and-kill-outdated-buffers 20)
-                             (session-save)))
+;;---------------------------------Package Management-------------------------------------
 
 ;; Initialize package manager
 (require 'package)
@@ -87,6 +77,10 @@
 (eval-and-compile
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
+
+(use-package cyberpunk-theme
+  :config
+  (add-hook 'after-init-hook (lambda () (load-theme 'cyberpunk t))))
 
 (use-package treemacs
   :config
