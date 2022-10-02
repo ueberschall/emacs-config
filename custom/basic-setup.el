5f5edfe320b2833069370e41d6f6ee2fdf00d06d
Nasser Attar <nasser.attar@hotmail.com>
2022-10-02 21:21:54 +0200

    Add org-roam package
    It seems it needs org in the version >= 9.4 to be working

--- custom/basic-setup.el
+++ custom/basic-setup.el
@@ -177,6 +177,25 @@
   (add-hook 'org-mode-hook (lambda ()
                              (org-superstar-mode 1))))
 
+(use-package org-roam
+  :after org
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
 ;; Configure company
 (use-package company
   :init
